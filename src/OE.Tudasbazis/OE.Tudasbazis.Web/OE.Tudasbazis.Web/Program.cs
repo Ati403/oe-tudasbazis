fc1965f08a48e7feb51732fdd02eabc9794de2ff
Nagy Attila <nagy.attisr.50@gmail.com>
2024-10-02 17:41:23 +0200

    Alap web alkalmazas front/backend

---
+++ src/OE.Tudasbazis/OE.Tudasbazis.Web/OE.Tudasbazis.Web/Program.cs
@@ -0,0 +1,52 @@
+using OE.Tudasbazis.Web.Client.Pages;
+using OE.Tudasbazis.Web.Client.Services;
+using OE.Tudasbazis.Web.Components;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+		builder.Services.AddControllers();
+		builder.Services.AddRazorComponents()
+			.AddInteractiveWebAssemblyComponents();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+		builder.Services.AddScoped(http => new HttpClient
+		{
+			BaseAddress = new Uri("Https://localhost:7165")
+		}) ;
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseWebAssemblyDebugging();
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+		else
+		{
+			app.UseExceptionHandler("/Error", createScopeForErrors: true);
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseStaticFiles();
+		app.UseAntiforgery();
+
+		app.MapRazorComponents<App>()
+			.AddInteractiveWebAssemblyRenderMode()
+			.AddAdditionalAssemblies(typeof(OE.Tudasbazis.Web.Client._Imports).Assembly);
+
+		app.MapControllers();
+
+		app.Run();
+	}
+}
