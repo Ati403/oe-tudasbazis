99259dd96609dbb3d63ed92d3d74c5e01877a087
Ábel Miklós <abel.miklos@stud.uni-obuda.hu>
2024-10-05 21:36:01 +0200

    Added EF to DataAccess

---
+++ src/OE.Tudasbazis/OE.Tudasbazis.DataAccess/DependencyInjection.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OE.Tudasbazis.DataAccess
+{
+    public static class DependencyInjection
+    {
+    }
+}
---
+++ src/OE.Tudasbazis/OE.Tudasbazis.DataAccess/TudasbazisDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace OE.Tudasbazis.DataAccess
+{
+	public class TudasbazisDbContext : DbContext
+	{
+
+	}
+}
