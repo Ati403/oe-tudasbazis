c817b9a18f557d9ef1dee5c8caea57db48b512a1
Ábel Miklós <abel.miklos@stud.uni-obuda.hu>
2024-10-19 14:05:23 +0200

    Add ElasticSearch integration for document indexing/search

---
+++ src/OE.Tudasbazis/OE.Tudasbazis.Application/Services/IElasticService.cs
@@ -0,0 +1,23 @@
+using OE.Tudasbazis.Application.DTOs.Service;
+using OE.Tudasbazis.Application.Exceptions;
+
+namespace OE.Tudasbazis.Application.Services
+{
+	public interface IElasticService
+	{
+		/// <summary>
+		///		Indexes the embedding of a document asynchronously.
+		/// </summary>
+		/// <param name="document">The ElasticDocument to be indexed.</param>
+		/// <exception cref="BusinessLogicException">Thrown when an error occurs during indexing.</exception>
+		Task IndexEmbeddingAsync(ElasticDocument document);
+
+        /// <summary>
+        /// Searches for the top K similar documents to the given query embedding.
+        /// </summary>
+        /// <param name="queryEmbedding">The query embedding to search for similar documents.</param>
+        /// <param name="topK">The number of similar documents to retrieve (default is 5).</param>
+        /// <returns>A list of ElasticDocuments representing the search results.</returns>
+        Task<List<ElasticDocument>> SearchAsync(float[] queryEmbedding, int topK = 5);
+	}
+}
