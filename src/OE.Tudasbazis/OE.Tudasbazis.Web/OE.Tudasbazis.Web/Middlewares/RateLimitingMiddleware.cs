595df19cc3c117c31a08aacedea599411ff719d4
Ábel Miklós <abel.miklos@stud.uni-obuda.hu>
2024-11-17 13:01:18 +0100

    Rate limiting middleware temp turn off

--- src/OE.Tudasbazis/OE.Tudasbazis.Web/OE.Tudasbazis.Web/Middlewares/RateLimitingMiddleware.cs
+++ src/OE.Tudasbazis/OE.Tudasbazis.Web/OE.Tudasbazis.Web/Middlewares/RateLimitingMiddleware.cs
@@ -25,43 +25,43 @@ namespace OE.Tudasbazis.Web.Middlewares
 
 		public async Task InvokeAsync(HttpContext context)
 		{
-			var endpoint = context.GetEndpoint();
+			//var endpoint = context.GetEndpoint();
 
-			if (endpoint == null
-				|| !RateLimitingHelper.LimitedEndpoints.Exists(endpoint.DisplayName!.Contains))
-			{
-				await _next(context);
-				return;
-			}
+			//if (endpoint == null
+			//	|| !RateLimitingHelper.LimitedEndpoints.Exists(endpoint.DisplayName!.Contains))
+			//{
+			//	await _next(context);
+			//	return;
+			//}
 
-			bool isAuthenticated = context.User.Identity?.IsAuthenticated ?? false;
-			if (isAuthenticated)
-			{
-				await _next(context);
-				return;
-			}
+			//bool isAuthenticated = context.User.Identity?.IsAuthenticated ?? false;
+			//if (isAuthenticated)
+			//{
+			//	await _next(context);
+			//	return;
+			//}
 
-			string? ipAddress = context.Connection.RemoteIpAddress?.ToString();
-			if (ipAddress is null)
-			{
-				context.Response.StatusCode = StatusCodes.Status403Forbidden;
-				await context.Response.WriteAsync("IP address not found.");
-				return;
-			}
+			//string? ipAddress = context.Connection.RemoteIpAddress?.ToString();
+			//if (ipAddress is null)
+			//{
+			//	context.Response.StatusCode = StatusCodes.Status403Forbidden;
+			//	await context.Response.WriteAsync("IP address not found.");
+			//	return;
+			//}
 
-			string cacheKey = $"RateLimit_{ipAddress}";
-			if (_chache.TryGetValue(cacheKey, out bool hasAccessed))
-			{
-				context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
-				await context.Response.WriteAsync("Be kell jelentkezni további kérések küldéséhez.");
-				return;
-			}
+			//string cacheKey = $"RateLimit_{ipAddress}";
+			//if (_chache.TryGetValue(cacheKey, out bool hasAccessed))
+			//{
+			//	context.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+			//	await context.Response.WriteAsync("Be kell jelentkezni további kérések küldéséhez.");
+			//	return;
+			//}
 
-			_chache.Set(cacheKey, true, new MemoryCacheEntryOptions
-			{
-				Priority = CacheItemPriority.Normal,
-				SlidingExpiration = TimeSpan.FromMinutes(10)
-			});
+			//_chache.Set(cacheKey, true, new MemoryCacheEntryOptions
+			//{
+			//	Priority = CacheItemPriority.Normal,
+			//	SlidingExpiration = TimeSpan.FromMinutes(10)
+			//});
 
 			await _next(context);
 		}
