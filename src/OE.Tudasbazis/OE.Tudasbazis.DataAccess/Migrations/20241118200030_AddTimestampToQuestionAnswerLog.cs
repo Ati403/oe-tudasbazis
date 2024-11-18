6b14dc5cacf78c8fa85d607a2119316d50179d7e
Ábel Miklós <abel.miklos@stud.uni-obuda.hu>
2024-11-18 21:06:07 +0100

    Created migration for QuestionAnswerLog entity modification

---
+++ src/OE.Tudasbazis/OE.Tudasbazis.DataAccess/Migrations/20241118200030_AddTimestampToQuestionAnswerLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OE.Tudasbazis.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimestampToQuestionAnswerLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "QuestionAnswerLogs",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "QuestionAnswerLogs");
+        }
+    }
+}
